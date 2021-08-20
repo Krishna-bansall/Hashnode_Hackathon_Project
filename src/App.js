69abe9dcccf4d208a01c802964335ef43154869c
nikk18 <nikhildhiman9050@gmail.com>
2021-08-20 16:34:57 +0530

    completed navbar/navmenu

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './App.css';
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './components/pages';
+import Feedback from './components/pages/feedback';
+import SignUp from './components/pages/signup';
+import SignIn from './components/pages/signin';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/feedback" component={Feedback} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/sign-up" component={SignUp} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/pages/feedback.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Feedback() {
+    return (
+        <div
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh'
+            }}
+        >
+            <h1>Share your feedback about our page!</h1> 
+        </div>
+    );
+};
+
+export default Feedback;
